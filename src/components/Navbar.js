c23e135ddc062a87430c1d7c52e92c0d34bc1c71
Bojan Nikoloski <nikolovski.bojan@students.finki.ukim.mk>
2024-11-15 18:07:56 +0100

    Fix Navbar accesability issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,13 +10,17 @@ const Navbar = () => {
 
   return (
     <header className="navbar">
-      <a href="#" className="logo">Reminder App </a>
+      {/* Updated the logo link to use React Router's Link */}
+      <Link to="/" className="logo">Reminder App</Link> 
+
       <nav className={`nav-links ${isOpen ? 'show' : ''}`}>
         <ul>
           <li><Link to="/">Words Reminder</Link></li>  
           <li><Link to="/coding" className="secound-nav">Code Reminder</Link></li>  
         </ul>
       </nav>
+
+      {/* Hamburger menu for mobile */}
       <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
         <span></span>
         <span></span>
